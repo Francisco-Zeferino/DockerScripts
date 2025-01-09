8d51dd8bae122e1fcb28e0e503dc7fb6cdae2273
Francisco Zeferino <58032894+Francisco-Zeferino@users.noreply.github.com>
2025-01-09 12:34:35 +0000

    Fixed some errors!

--- srcs/requirements/wordpress/setup/setup.sh
+++ srcs/requirements/wordpress/setup/setup.sh
@@ -4,18 +4,23 @@ sleep 10
 
 cd "$WP_PATH"
 
-wp core download --allow-root 
+if [ -d "/var/www/html/ffilipe-.42.fr/wp-admin" ]
+then
+    echo "Wordpress already installed, waiting for mariadb to start..."
+    sleep 10
+else
+    wp core download --allow-root 
 
-wp core install --allow-root --url=$WP_URL \
-    --title=$WP_TITLE \
-    --admin_user=$WP_USER \
-    --admin_password=$WP_PASSWORD \
-    --admin_email=$WP_EMAIL
+    wp core install --allow-root --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_USER \
+        --admin_password=$WP_PASSWORD \
+        --admin_email=$WP_EMAIL
 
-wp user create --allow-root \
-    $WP_SECOND_USER $WP_SECOND_EMAIL \
-    --user_pass=$WP_SECOND_USER_PASSWORD
+    wp user create --allow-root \
+        $WP_SECOND_USER $WP_SECOND_EMAIL \
+        --user_pass=$WP_SECOND_USER_PASSWORD
 
-wp theme activate twentytwentyfive --allow-root
-
+fi
+echo "Wordpress is online!"
 exec $@
